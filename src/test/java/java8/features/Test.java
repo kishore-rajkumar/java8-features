cbd848a86102c9532bb89cd0717bb11a85a9108c
Kishore <kishore.rajkumar@hotmail.com>
2021-10-23 10:53:53 +0530

    Test class added

---
+++ src/test/java/java8/features/Test.java
@@ -0,0 +1,11 @@
+package java8.features;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+//		fail("Not yet implemented");
+	}
+
+}
